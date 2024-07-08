ef8c027d6a2c274b2f460f2eeb36f904084ca1ef
liangyancheng <656040874@qq.com>
2024-07-08 11:20:53 +0800

    feat: :sparkles: init project

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+// 导入重置样式
+// import './styles/reset.css'
+
+const app = createApp(App)
+
+app.mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    open: false,
+    port: 5188,
+  },
+})
